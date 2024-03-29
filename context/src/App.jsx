fcdd6b4541df9136c16b8e95ad6ae9c868cd2ca5
sambit <sambitbinakar@gmail.com>
2024-03-29 08:40:29 +0530

    contextapi

--- context/src/App.jsx
+++ context/src/App.jsx
@@ -1,13 +1,18 @@
 
-import './App.css'
 
+import UserContextProvider from './Context/Usercontextprovider'
+import Login from './components/Login'
+import Profile from './components/Profile'
+import './App.css'
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
+    <UserContextProvider>
    <h1>React -Context -API</h1>
-    </>
+   <Login/>
+   <Profile/>
+    </UserContextProvider>
   )
 }
 
